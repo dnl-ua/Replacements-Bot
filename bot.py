373094cf9db3974eb56e474266aaeb76400b1595
daniel <d.kagarlytskiy@gmail.com>
2021-01-14 09:19:01 +0200

    Adding bot script

---
+++ bot.py
@@ -0,0 +1,30 @@
+import os
+import urllib.request
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from config import TOKEN, URL, IMAGE_PATH
+
+
+url = URL
+urllib.request.urlretrieve(url, IMAGE_PATH)
+
+api_token = TOKEN
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=api_token)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+        await message.reply("Чтобы получить список замен введите команду /send");
+
+
+@dp.message_handler(commands=['send'])
+async def send(message: types.Message):
+        with open(IMAGE_PATH, 'rb') as photo:
+                await message.reply_photo(photo)
+
+
+if __name__ == '__main__':
+        executor.start_polling(dp, skip_updates=True)
